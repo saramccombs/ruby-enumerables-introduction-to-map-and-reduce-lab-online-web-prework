4299615a03ffe67d9ed1607fe0094ea1893cbf1e
Sara McCombs <thesaramccombs@gmail.com>
2019-08-06 22:04:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,11 +56,12 @@ def reduce_to_all_true(source_array)
   index = 0 
   while source_array[index] do
     return false if !source_array[index]
+    index += 1 
   end
   return true 
 end
 
 def reduce_to_any_true(source_array)
-  value = 0 
+  index = 0 
   
 end
