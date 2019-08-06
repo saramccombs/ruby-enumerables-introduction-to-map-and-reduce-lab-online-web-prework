7783269981aacf9bcfade0fa1f2af5e3a3001ace
Sara McCombs <thesaramccombs@gmail.com>
2019-08-06 21:50:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,63 @@
-# My Code here....
+def map_to_negativize(source_array)
+  
+  array_negatized = []
+
+  source_array.length.times do |index|
+    array_negatized[index] = source_array[index] * -1
+  end
+  
+  return array_negatized
+end
+
+def map_to_no_change(source_array)
+  
+  array_no_change = []
+  
+  source_array.length.times do |index|
+    array_no_change[index] = source_array[index]
+  end
+  
+  return array_no_change
+end
+
+def map_to_double(source_array)
+  
+  array_doubled = []
+  
+  source_array.length.times do |index|
+    array_doubled[index] = source_array[index] * 2 
+  end
+  
+  return array_doubled
+end
+
+def map_to_square(source_array)
+  array_squared = []
+  
+  source_array.length.times do |index|
+    array_squared[index] = source_array[index] ** 2 
+  end
+
+  return array_squared
+end
+
+def reduce_to_total(source_array)
+
+  total = 0 
+
+  source_array.length.times do |index|
+    total = total + source_array[index]
+  end
+  
+  return total
+end
+
+def reduce_to_all_true(source_array)
+  value = 0 
+ 
+end
+
+def reduce_to_any_true(source_array)
+  value = 0 
+  
+end
