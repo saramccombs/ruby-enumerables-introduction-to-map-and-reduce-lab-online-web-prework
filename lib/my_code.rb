3410a47be5693647f3b96c202b47de139fced906
Sara McCombs <thesaramccombs@gmail.com>
2019-08-06 22:05:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,9 +41,9 @@ def map_to_square(source_array)
   return array_squared
 end
 
-def reduce_to_total(source_array)
+def reduce_to_total(source_array, starting_point)
 
-  total = 0 
+  total = starting_point
 
   source_array.length.times do |index|
     total = total + source_array[index]
